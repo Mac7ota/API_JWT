8626ff6745d457726d95485dfd305ba954f7786e
Mauro Alves Aguiar <mauro.alves.aguiar@outlook.com>
2024-02-21 16:40:54 -0300

    configuração do TokenService

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using DefaultNamespace.Interfaces;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace DefaultNamespace;
 
@@ -19,6 +24,26 @@ public class TokenService : ITokenService
         if (user.UserName != _userDataBase.UserName && user.Password != _userDataBase.Password)
            return String.Empty;
         
+       var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? String.Empty));
        
+       var issuer = _configuration["Jwt:Issuer"] ?? String.Empty;
+       var audience = _configuration["Jwt:Audience"] ?? String.Empty;
+       
+       var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+       
+       var tokensOptions = new JwtSecurityToken(
+           issuer: issuer,
+           audience: audience,
+           claims: new []
+           {
+               new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role)
+           },
+           expires: DateTime.Now.AddMinutes(60),
+           signingCredentials: signingCredentials
+       );
+       
+       var token = new JwtSecurityTokenHandler().WriteToken(tokensOptions);
+       return token;
     }
 }
